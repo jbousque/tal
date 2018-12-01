9418df603c32845a802aaa96801943472dcdab4f
Jeremie Bousquet <jeremie.bousquet@gmail.com>
2018-12-01 12:25:28 +0100

    Update gen_all_data.py
    new data gen - update japanese paths

--- src/gen_all_data.py
+++ src/gen_all_data.py
@@ -10,19 +10,19 @@ for lang in ['fr', 'nl', 'en', 'ja']: # ja only has a test set ?!
         
             if lang == 'fr':
                 
-                conllu_name = "..\\data\\ud\\UD_French-GSD\\fr_gsd-ud-{phase}.conllu".format(phase=phase)
+                conllu_name = "..\\UD_French-GSD\\fr_gsd-ud-{phase}.conllu".format(phase=phase)
             
             elif lang == 'nl':
             
-                conllu_name = "..\\data\\ud\\UD_Dutch-LassySmall\\nl_lassysmall-ud-{phase}.conllu".format(phase=phase)
+                conllu_name = "..\\UD_Dutch-LassySmall\\nl_lassysmall-ud-{phase}.conllu".format(phase=phase)
                 
             elif lang == 'en':
                 
-                conllu_name = "..\\data\\ud\\UD_English-LinES\\en_lines-ud-{phase}.conllu".format(phase=phase)
+                conllu_name = "..\\UD_English-LinES\\en_lines-ud-{phase}.conllu".format(phase=phase)
                 
             elif lang == 'ja':
                 
-                conllu_name = "..\\data\\ud\\UD_Japanese-GSD\\ja_gsd-ud-{phase}.conllu".format(phase=phase)
+                conllu_name = "..\\UD_Japanese-GSD\\ja_gsd-ud-{phase}.conllu".format(phase=phase)
             
             output_name = "{f}_{lang}-{phase}".format(f=f, lang=lang, phase=phase)
         
